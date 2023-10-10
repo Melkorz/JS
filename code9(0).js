b85d06f6b4dbb434df53466d741c4279b90f04cb
Melkorz <djeday2000a@yandex.ru>
2023-10-10 13:11:00 +0500

    9 Chapter

--- code9(0).js
+++ code9(0).js
@@ -9,3 +9,30 @@ function ticker(){
     }
 }
 setInterval(ticker, 1000);
+
+var addOne = function (x) {
+    return x + 1;
+};
+var six = addOne(5);
+console.log(six); // 6
+
+// Page 453
+var migrating = true;
+var fly = function(num){
+    for(let i = 0; i < num; i++){
+        console.log("Flying");
+    }
+};
+
+function quack(num){
+    for(let i = 0; i < num; i++){
+        console.log("Quack")
+    }
+}
+
+if(migrating){
+    quack(4);
+    fly(4);
+}
+
+// Page 
