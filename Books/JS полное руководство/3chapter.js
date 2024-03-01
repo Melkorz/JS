d13c8d145946d4281186df74c56a7c5683ef8657
Melkorz <djeday2000a@yandex.ru>
2024-03-01 16:00:31 +0500

    3 Chapter

---
+++ Books/JS полное руководство/3chapter.js
@@ -0,0 +1,14 @@
+//Page 72
+console.log(Number("3")); // => 3 
+console.log(String(false)); // => "false": либо используйте false.toString() 
+console.log(Boolean([])); // => true
+
+//Page 73
+var п = 17;
+let binary = "Ob" + n.toString(2); 
+console.log(binary); // binary == "OblOOOl" 
+let octal = "Oo" + n.toString(8); 
+console.log(octal); // octal == "0o21” 
+let hex = "Ox" + n.toString(16); 
+console.log(hex); // hex == "0x11"
+
